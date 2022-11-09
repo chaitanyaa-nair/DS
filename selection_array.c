2aa97b5aaf0bf9d2dab58d37a86c5af2ee7b1d0d
Chaitanyaa Nair <chaitanyaa.nair_ug23@ashoka.edu.in>
2022-11-09 16:41:39 +0530

    updates:

--- selection_array.c
+++ selection_array.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+void selection (int A[], int n);
+void swap (int *with, int *to);
+
 int main (int n, int i){
     //create an array using user input
 
@@ -10,40 +13,48 @@ int main (int n, int i){
     if (n <= 0){
         printf("Error, please enter a non-zero positive number.");
     }
-    else{
-        int arr[n];
-    }
-
+    int arr[n];
     //taking inputs into the array
     printf("Enter the elements:");
     for (i=0; i<n; i++){
         scanf ("%d", &arr[i]);
         }
-    printf("The unsorted array is:\n");
+    printf("\n The unsorted array is:\n");
         
     for (i = 0; i < n; i++){
-        printf("%d,", arr[i]);
+        printf("%d\n", arr[i]);
         }
-    selection(arr[n], n);
     
-    printf("The sorted array is:\n");
+    selection(arr, n);
+    
+    printf("\n The sorted array is:\n");
         
     for (i = 0; i < n; i++){
-        printf("%d,", arr[i]);
+        printf("%d\n", arr[i]);
         }
     }
 
 
 //devise the selection sort algorithm 
-int selection(int A[n], int n){
-    int i, temp;
-    for (i; i < n-1; i++){
+void selection(int A[], int n){
+    int i;
+    for (i = 0; i < n-1; i++){
+        int min = i;
         //check for the minimum element
-        //if smallest element, swap with the first position
+        int j;
+        for (j = i + 1; j < n; j++){
+            if (A[j] < A[min]){
+                min = j;
+            }
+            //if smallest element, swap with the first position
+            }
+            swap (&A[min], &A[i]);
     }
 }
 
-int swap(int first, int end, int A[n], int n){
+void swap(int *with, int *to){
     //swapping elements to place at the beginning of the list
-    
+    int current = *with; //temporary variable for the element to be swapped with
+    *with = *to; //assigning the position of the element to be swapped with to the element to be swapped to
+    *to = current; //reassigning the swapped element to the current or with elements
 }
