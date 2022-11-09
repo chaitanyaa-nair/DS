14cc2762bf92bc8b534760414df2cd515e76df0b
Chaitanyaa Nair <chaitanyaa.nair_ug23@ashoka.edu.in>
2022-11-09 22:49:31 +0530

    update

--- insertion_array.c
+++ insertion_array.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+void insertion(int arr[], int n);
+
 int main (int n, int i){
     //create an array using user input
 
@@ -10,21 +12,42 @@ int main (int n, int i){
     if (n <= 0){
         printf("Error, please enter a non-zero positive number.");
     }
-    else{
-        int arr[n];
-    }
 
+    int arr[n];
     //taking inputs into the array
+    printf("Enter the elements:");
     for (i=0; i<n; i++){
         scanf ("%d", &arr[i]);
         }
-        printf("The array is:\n");
+    printf("\n The unsorted array is:\n");
         
-        for (i = 0; i < n; i++){
-            printf("%d\n", arr[i]);
-            }
+    for (i = 0; i < n; i++){
+        printf("%d\n", arr[i]);
+        }
+    
+    insertion(arr, n);
+    
+    printf("\n The sorted array is:\n");
+        
+    for (i = 0; i < n; i++){
+        printf("%d\n", arr[i]);
+        }
     }
 
-//devise the insertion sort algorithm 
+//devise alogorithm for insertion sort
+void insertion(int arr[], int n) {
+    //for every element, check if the next element is either greater or smaller
+    for (int i = 1; i < n; i++) {
+    int key = arr[i];
+    int j = i - 1;
+
+    // Compare key with each element on the left of it until an element smaller than it is found.
+    while (key < arr[j] && j >= 0) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+}
 
 
